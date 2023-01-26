0a3fcd8d7b17fa3bb6ba632a32d9f33ec4d72f9e
Kira Kopcho <kirak5683@gmail.com>
2023-01-25 20:39:28 -0800

    clean up formatting for string printing

--- rover_gps/serial_test.py
+++ rover_gps/serial_test.py
@@ -5,5 +5,6 @@ port = serial.Serial('/dev/ttyACM0')
 print(port.name) # check port name to make sure it's opened correctly
 
 while 1:
-    line = port.readline() #read line until newline char
-    print(line) #print response
+    line = port.read(100) #read size of buffer
+    final = line.decode("utf-8")
+    print(final) #print response

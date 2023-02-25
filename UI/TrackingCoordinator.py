b3ca9c8f48d6d9fca254b5dd16c11e208faf154c
Kira Kopcho <kirak5683@gmail.com>
2023-02-25 13:56:06 -0800

    Got tracking algo class properly imported, start work on signals and slots

--- UI/TrackingCoordinator.py
+++ UI/TrackingCoordinator.py
@@ -1,12 +1,21 @@
 from PyQt5 import QtWidgets, QtCore, QtGui, uic
 import sys
-#import tracking_algorithm
+#get path to tracking algorithm
+sys.path.insert(1, '/home/groundstation/github/OSURC-RDF-Tracking-Algo/rover_gps')
+import tracking_algorithm
 
 class TrackingCore(QtWidgets.QWidget):
     def __init__(self):
         super(TrackingCore, self).__init__()
+        trackingAlgo = tracking_algorithm.TrackingAlgorithm()
         uic.loadUi('TrackingAlgoDisp.ui', self)
         self.show()
+
+	self.rover_lat = QLabel()
+	self.rover_lon = QLabel()
+
+    def on_update_rover_coordinates_ready(self, algo):
+        
 
 app = QtWidgets.QApplication(sys.argv)
 window = TrackingCore()

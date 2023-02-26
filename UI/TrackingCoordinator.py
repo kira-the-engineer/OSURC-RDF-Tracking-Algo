bbd1c37e7b544c0e639b4aa43c117b861afca676
Kira Kopcho <kirak5683@gmail.com>
2023-02-26 14:48:34 -0800

    thread started successfully and that is the correct implementation, however the UI isn't properly recieving updated data from the tracking algo

--- UI/TrackingCoordinator.py
+++ UI/TrackingCoordinator.py
@@ -6,23 +6,26 @@ import tracking_algorithm
 
 #create threaded class to avoid blocking UI updates
 class TrackingCore(QtCore.QThread):
+	print("Tracking Thread Started!")
 	#create signals
 	rover_lat_update_ready__signal = QtCore.pyqtSignal(float)
 	rover_lon_update_ready__signal = QtCore.pyqtSignal(float)
 
 	def __init__(self):
 		super(TrackingCore,self).__init__()
-		self.rover_lat = QLabel()
-		self.rover_lon = QLabel()
-
 		## Class Variables ##
 		self.current_rover_lat = -1
 		self.current_rover_lon = -1
 
 	def rover_latitude_changed__slot(self, trackingAlgo):
 		self.current_rover_lat = trackingAlgo.r_lat
-		self.rover_lat_update_ready__signal.emit(current_rover_lat)
+		self.rover_lat_update_ready__signal.emit(self.current_rover_lat)
 
 	def rover_longitide_changed__slot(self, trackingAlgo):
 		self.current_rover_lon = trackingAlgo.r_lon
-		self.rover_lon_update_ready__signal.emit(current_rover_lon)
+		self.rover_lon_update_ready__signal.emit(self.current_rover_lon)
+		
+	def run(self):
+		while 1:
+			tAlgo = tracking_algorithm.TrackingAlgorithm()
+			self.rover_latitude_changed__slot(tAlgo)
--- UI/TrackingUI.py
+++ UI/TrackingUI.py
@@ -1,12 +1,19 @@
 from PyQt5 import QtWidgets, QtCore, QtGui, uic
 import sys
+import TrackingCoordinator as track
 
 class TrackingUI(QtWidgets.QWidget):
     def __init__(self):
         super(TrackingUI, self).__init__()
         uic.loadUi('TrackingAlgoDisp.ui', self)
-        self.show()
+        self.tThread = track.TrackingCore()
+        self.tThread.rover_lat_update_ready__signal.connect(self.updateRLat)
+        self.tThread.start()
+	
+    def updateRLat(self, value):
+    	self.rover_lat.setNum(value)
 
 app = QtWidgets.QApplication(sys.argv)
 window = TrackingUI()
+window.show()
 app.exec_()

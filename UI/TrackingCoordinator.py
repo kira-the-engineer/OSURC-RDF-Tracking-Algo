ca31e048689efb9078dfac0d17296ca0bc8ae1d8
Kira Kopcho <kirak5683@gmail.com>
2023-02-26 16:49:30 -0800

    spoofing coordinate data for now, need to put more research into how to connect tracking algo to UI but that's not necessary for demo

--- UI/TrackingCoordinator.py
+++ UI/TrackingCoordinator.py
@@ -1,8 +1,10 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 import sys
-#get path to tracking algorithm
-sys.path.insert(1, '/home/groundstation/github/OSURC-RDF-Tracking-Algo/rover_gps')
-import tracking_algorithm
+import random
+from time import sleep
+
+#Dearborn Hall coordinates: 44.56688122224506, -123.27560553741544
+#Near Merryfield coordinates: 44.566890589052235, -123.27462028171236
 
 #create threaded class to avoid blocking UI updates
 class TrackingCore(QtCore.QThread):
@@ -10,22 +12,47 @@ class TrackingCore(QtCore.QThread):
 	#create signals
 	rover_lat_update_ready__signal = QtCore.pyqtSignal(float)
 	rover_lon_update_ready__signal = QtCore.pyqtSignal(float)
+	base_lat_update_ready__signal = QtCore.pyqtSignal(float)
+	base_lon_update_ready__signal = QtCore.pyqtSignal(float)
 
 	def __init__(self):
 		super(TrackingCore,self).__init__()
 		## Class Variables ##
 		self.current_rover_lat = -1
 		self.current_rover_lon = -1
-
-	def rover_latitude_changed__slot(self, trackingAlgo):
-		self.current_rover_lat = trackingAlgo.r_lat
+		self.current_base_lat = -1
+		self.current_base_lon = -1
+		
+	def get_random_coordinate(self, start_val, stop_val):
+		coord = random.uniform(start_val, stop_val)
+		return coord
+	
+	def rover_latitude_changed__slot(self):
+		#creating false coordinates for verification purposes, actual final code will take in input from tracking algo
+		self.current_rover_lat = self.get_random_coordinate(42.56688, 48.2333)
 		self.rover_lat_update_ready__signal.emit(self.current_rover_lat)
+		sleep(0.2)
 
-	def rover_longitide_changed__slot(self, trackingAlgo):
-		self.current_rover_lon = trackingAlgo.r_lon
+	def rover_longitude_changed__slot(self):
+		self.current_rover_lon = self.get_random_coordinate(-120.27560, -125.3241)
 		self.rover_lon_update_ready__signal.emit(self.current_rover_lon)
+		sleep(0.2)
+	
+	def base_latitude_changed__slot(self):
+		self.current_base_lat = self.get_random_coordinate(40.5668905, 46.323212)
+		self.base_lat_update_ready__signal.emit(self.current_base_lat)
+		sleep(0.2)
+		
+
+	def base_longitude_changed__slot(self):
+		self.current_base_lon = self.get_random_coordinate(-119.274620, -125.274620)
+		self.base_lon_update_ready__signal.emit(self.current_base_lon)
+		sleep(0.2)
 		
 	def run(self):
 		while 1:
-			tAlgo = tracking_algorithm.TrackingAlgorithm()
-			self.rover_latitude_changed__slot(tAlgo)
+			self.rover_latitude_changed__slot()
+			self.rover_longitude_changed__slot()
+			self.base_latitude_changed__slot()
+			self.base_longitude_changed__slot()
+			
--- UI/TrackingUI.py
+++ UI/TrackingUI.py
@@ -3,15 +3,27 @@ import sys
 import TrackingCoordinator as track
 
 class TrackingUI(QtWidgets.QWidget):
-    def __init__(self):
-        super(TrackingUI, self).__init__()
-        uic.loadUi('TrackingAlgoDisp.ui', self)
-        self.tThread = track.TrackingCore()
-        self.tThread.rover_lat_update_ready__signal.connect(self.updateRLat)
-        self.tThread.start()
+	def __init__(self):
+		super(TrackingUI, self).__init__()
+		uic.loadUi('TrackingAlgoDisp.ui', self)
+		self.tThread = track.TrackingCore()
+		self.tThread.rover_lat_update_ready__signal.connect(self.updateRLat)
+		self.tThread.rover_lon_update_ready__signal.connect(self.updateRLon)
+		self.tThread.base_lat_update_ready__signal.connect(self.updateBLat)
+		self.tThread.base_lon_update_ready__signal.connect(self.updateBLon)
+		self.tThread.start()
 	
-    def updateRLat(self, value):
-    	self.rover_lat.setNum(value)
+	def updateRLat(self, value):
+		self.rover_lat.setNum(value)
+    	
+	def updateRLon(self, value):
+		self.rover_lon.setNum(value)
+		
+	def updateBLat(self, value):
+		self.base_lat.setNum(value)
+		
+	def updateBLon(self, value):
+		self.base_lon.setNum(value)
 
 app = QtWidgets.QApplication(sys.argv)
 window = TrackingUI()
